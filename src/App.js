32d76718cabee5050142fe5f8c5aac97d790a4d0
kashif2666 <kashifjsr7@gmail.com>
2024-04-26 14:21:08 +0530

    Cart Page added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import Cart from "./features/cart/Cart";
 import ProductList from "./features/product-list/ProductList";
+import CartPage from "./pages/CartPage";
 import Home from "./pages/Home";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
@@ -24,6 +26,10 @@ const router = createBrowserRouter([
   {
     path: "/signup",
     element: <SignupPage></SignupPage>,
+  },
+  {
+    path: "/cart",
+    element: <CartPage></CartPage>,
   },
 ]);
 
